525b8d97d1d898d4fc724828d90524e484c6a02e
Adrian Maciej Osedowski <adrian.osedowski@students.salle.url.edu>
2023-05-21 20:28:50 +0200

    show all informations for given user input

--- show_countries/views.py
+++ show_countries/views.py
@@ -1,3 +1,7 @@
+import json
+import random
+import re
+
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -13,23 +17,26 @@ def home(request):
         number = int(number_str)
         print("requested")
 
-        countries_list = retrieve_region(continent)
-        if (countries_list == 0):
-            print("Continent name doesn't match any region")
-            validation_message = "Continent name does not match any region"
-            return render(request, 'home.html', {'validation_message': validation_message})
-
         if (number < 2 or number >10):
             print("Number should be between 2 and 10")
             validation_message = "Number should be between 2 and 10"
             return render(request, 'home.html', {'validation_message': validation_message})
 
-        return render(request, 'home.html')
+        countries_list = retrieve_region(continent, number)
+
+        if (countries_list == 0):
+            print("Continent name doesn't match any region")
+            validation_message = "Continent name does not match any region"
+            return render(request, 'home.html', {'validation_message': validation_message})
+
+        countries_info = retrieve_country_info(countries_list)
+
+        return render(request, 'home.html', {'countries': countries_info})
     else:
         return render(request, 'home.html')
 
 
-def retrieve_region(region):
+def retrieve_region(region, number):
     regions = {
         "europe": "EU",
         "asia": "AS",
@@ -55,7 +62,7 @@ def retrieve_region(region):
     query Query($code: ID!) {
         continent(code: $code) {
             countries {
-                code
+                name
         }
       }
     }
@@ -70,9 +77,45 @@ def retrieve_region(region):
 
     response = requests.post(url=url, json=payload)
 
-    result = response.text
+    result_json = response.text
+    result = json.loads(result_json)
+    countries_len = len(result['data']['continent']['countries'])
     print(response.status_code)
     print(result)
-    return result
+    countries_arr = []
+    for i in range(number):
+        random_number = random.randint(0, countries_len-1)
+        country = result['data']['continent']['countries'][random_number]['name']
+        country_new = re.sub(r'\[.*?\]', '', country)
+        countries_arr.append(country_new)
+    print(countries_arr)
 
+    return countries_arr
 
+def retrieve_country_info(countries_arr):
+    ret_arr = []
+    i = 0
+    for country in countries_arr:
+        in_arr = []
+        url = "https://restcountries.com/v3.1/name/{}".format(country)
+        response_json = requests.get(url=url)
+        result = json.loads(response_json.text)
+        print(result[0]['name']['official'])
+        currencies = result[0]['currencies']
+        currency_names = [value['name'] for value in currencies.values()]
+        languages = result[0]['languages']
+        languages_names = list(languages.values())
+        country = {
+            'name': result[0]['name']['official'],
+            'capital': result[0]['capital'][0],
+            'population': result[0]['population'],
+            'currencies': currency_names,
+            'subregion': result[0]['subregion'],
+            'languages': languages_names
+        }
+        i += 1
+
+        ret_arr.append(country)
+
+    print(ret_arr)
+    return ret_arr
