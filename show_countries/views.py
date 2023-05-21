bba166a585fb01cacf776760da6a06924b2394c5
Adrian Maciej Osedowski <adrian.osedowski@students.salle.url.edu>
2023-05-21 22:20:08 +0200

    fix

--- show_countries/views.py
+++ show_countries/views.py
@@ -3,30 +3,27 @@ import random
 import re
 
 import requests
-from django.http import HttpResponse
 from django.shortcuts import render
-# Create your views here.
-from django.views.generic import CreateView
-
 
 def home(request):
-
     if request.method == 'POST':
         continent = request.POST.get('continent')
         number_str = request.POST.get('number')
         number = int(number_str)
         print("requested")
 
-        if (number < 2 or number >10):
+        # check if given number is in range from 2 to 10
+        if (number < 2 or number > 10):
             print("Number should be between 2 and 10")
             validation_message = "Number should be between 2 and 10"
             return render(request, 'home.html', {'validation_message': validation_message})
 
         countries_list = retrieve_region(continent, number)
 
+        # check if given continent name exist
         if (countries_list == 0):
             print("Continent name doesn't match any region")
-            validation_message = "Continent name does not match any region"
+            validation_message = "Continent does not exist"
             return render(request, 'home.html', {'validation_message': validation_message})
 
         countries_info = retrieve_country_info(countries_list)
@@ -44,20 +41,21 @@ def retrieve_region(region, number):
         "antarctica": "AN",
         "north america": "NA",
         "oceania": "OC",
-        "south africa": "SA",
+        "south america": "SA",
     }
+    # user input to lowercase letters
     lowercase_reg = region.lower()
 
+    # check if user input match possible continents
     try:
         region_code = regions[lowercase_reg]
     except:
         print("Region not exist")
         return 0
 
-
-
     url = 'https://countries.trevorblades.com/graphql'
 
+    # graphql query to take country names
     query = '''
     query Query($code: ID!) {
         continent(code: $code) {
@@ -79,41 +77,67 @@ def retrieve_region(region, number):
 
     result_json = response.text
     result = json.loads(result_json)
+    # number of all countries in region
     countries_len = len(result['data']['continent']['countries'])
     print(response.status_code)
     print(result)
     countries_arr = []
-    for i in range(number):
-        random_number = random.randint(0, countries_len-1)
-        country = result['data']['continent']['countries'][random_number]['name']
+
+    # get random countries
+    random_numbers = random.sample(range(0, countries_len - 1), number)
+    for i in random_numbers:
+        country = result['data']['continent']['countries'][i]['name']
         country_new = re.sub(r'\[.*?\]', '', country)
         countries_arr.append(country_new)
+    # sort countries in alphabetical order
+    sorted_countries = sorted(countries_arr)
     print(countries_arr)
 
-    return countries_arr
+    return sorted_countries
+
 
 def retrieve_country_info(countries_arr):
     ret_arr = []
-    i = 0
     for country in countries_arr:
-        in_arr = []
         url = "https://restcountries.com/v3.1/name/{}".format(country)
         response_json = requests.get(url=url)
         result = json.loads(response_json.text)
-        print(result[0]['name']['official'])
-        currencies = result[0]['currencies']
-        currency_names = [value['name'] for value in currencies.values()]
-        languages = result[0]['languages']
-        languages_names = list(languages.values())
+        try:
+            name = result[0]['name']['official']
+        except:
+            name = country
+        try:
+            capital = result[0]['capital'][0]
+        except:
+            capital = "No information found!"
+        try:
+            population = result[0]['population']
+        except:
+            population = "No information found!"
+        try:
+            currencies = result[0]['currencies']
+            currency_names = [value['name'] for value in currencies.values()]
+        except:
+            currency_names[0] = "No information found!"
+        try:
+            subregion = result[0]['subregion']
+        except:
+            subregion = "No information found!"
+        try:
+            languages = result[0]['languages']
+            languages_names = list(languages.values())
+        except:
+            languages_names[0] = "No information found!"
+
+        # dict with country and information
         country = {
-            'name': result[0]['name']['official'],
-            'capital': result[0]['capital'][0],
-            'population': result[0]['population'],
+            'name': name,
+            'capital': capital,
+            'population': population,
             'currencies': currency_names,
-            'subregion': result[0]['subregion'],
+            'subregion': subregion,
             'languages': languages_names
         }
-        i += 1
 
         ret_arr.append(country)
 
